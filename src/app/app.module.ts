4e351b9e15bd229ee65348052892dcbb92e9d519
Vinícius <santsmarcos81@gmail.com>
2024-11-20 13:48:26 -0300

    Criacao do projeto e implementacao do componente header

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { HomeComponent } from "./views/home/home.component";
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+  ],
+})
+
+export class AppModule { }
